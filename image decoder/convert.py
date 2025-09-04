a1c6b367f08169bb940d160d7cddf9fa46dcc8b0
Mast3rShak3 <mast3rshak305@gmail.com>
2025-09-04 12:57:15 -0700

    Update convert.py

--- image decoder/convert.py
+++ image decoder/convert.py
@@ -1,7 +1,7 @@
 import re
 
 # Read your header file
-with open("background_image.h", "r") as f:
+with open("input_image.h", "r") as f:
     data = f.read()
 
 # Find all hex values (0x..)
@@ -11,7 +11,8 @@ hex_values = re.findall(r"0x[0-9A-Fa-f]{2}", data)
 byte_data = bytes(int(h, 16) for h in hex_values)
 
 # Save to PNG
-with open("output_BG.png", "wb") as f:
+with open("output_image.png", "wb") as f:
     f.write(byte_data)
 
-print("Image saved as output.png")
+
+print("Image saved as output_image.png")
