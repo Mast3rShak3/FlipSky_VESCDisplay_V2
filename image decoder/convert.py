6c478cedc43e216bf67b93bcb6b0a4f6cf31df1a
Mast3rShak3 <mast3rshak305@gmail.com>
2025-09-04 12:55:36 -0700

    Add files via upload

---
+++ image decoder/convert.py
@@ -0,0 +1,17 @@
+import re
+
+# Read your header file
+with open("background_image.h", "r") as f:
+    data = f.read()
+
+# Find all hex values (0x..)
+hex_values = re.findall(r"0x[0-9A-Fa-f]{2}", data)
+
+# Convert to bytes
+byte_data = bytes(int(h, 16) for h in hex_values)
+
+# Save to PNG
+with open("output_BG.png", "wb") as f:
+    f.write(byte_data)
+
+print("Image saved as output.png")
